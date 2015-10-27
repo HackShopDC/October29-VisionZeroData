2fe7f98f0e5ca9ebdb5d98336091e30de46b10d9
katerabinowitz <cathryn.rabinowitz@gmail.com>
2015-10-26 20:49:25 -0400

    read csv from gh

--- StarterCode.R
+++ StarterCode.R
@@ -1,18 +1,17 @@
 library(rgdal)
 library(plyr)
-setwd ("/Users/katerabinowitz/Documents/HackShopDC/October29-VisionZeroData")
+### Read in Data ###
+### Read in Data ###
+### Read in Data ###
 
-### Read in Data ###
-### Read in Data ###
-### Read in Data ###
-# If you haven't read a GeoJson into R then see if 
+# If you haven't read a GeoJson into R before then see if 
 # "GeoJSON" %in% ogrDrivers()$name 
 # returns TRUE, if not you'll want to consider another format
-
 VisZeroMap = readOGR("http://opendata.dc.gov/datasets/3f28bc3ad77f49079efee0ac05d8464c_0.geojson", "OGRGeoJSON")
 
-VisZeroDF<-read.csv('Vision_Zero_Safety_Transportation.csv',na.strings=c("", "NA"),strip.white=TRUE,
+VisZeroDF<-read.csv('https://raw.githubusercontent.com/HackShopDC/October29-VisionZeroData/master/Vision_Zero_Safety_Transportation.csv',na.strings=c("", "NA"),strip.white=TRUE,
                     colClasses=c(rep("numeric",3),rep("character",5),"numeric","character","character"))
+
 count(VisZeroDF$USERTYPE)
 count(VisZeroDF$REQUESTTYPE)
 
