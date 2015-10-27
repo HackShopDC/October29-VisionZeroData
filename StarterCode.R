5c00554c436f07221c93d8ced50eb5ce6cfce868
katerabinowitz <cathryn.rabinowitz@gmail.com>
2015-10-26 20:47:25 -0400

    starter R code

---
+++ StarterCode.R
@@ -0,0 +1,21 @@
+library(rgdal)
+library(plyr)
+setwd ("/Users/katerabinowitz/Documents/HackShopDC/October29-VisionZeroData")
+
+### Read in Data ###
+### Read in Data ###
+### Read in Data ###
+# If you haven't read a GeoJson into R then see if 
+# "GeoJSON" %in% ogrDrivers()$name 
+# returns TRUE, if not you'll want to consider another format
+
+VisZeroMap = readOGR("http://opendata.dc.gov/datasets/3f28bc3ad77f49079efee0ac05d8464c_0.geojson", "OGRGeoJSON")
+
+VisZeroDF<-read.csv('Vision_Zero_Safety_Transportation.csv',na.strings=c("", "NA"),strip.white=TRUE,
+                    colClasses=c(rep("numeric",3),rep("character",5),"numeric","character","character"))
+count(VisZeroDF$USERTYPE)
+count(VisZeroDF$REQUESTTYPE)
+
+VisZeroDF$DATE <- as.Date(substr(VisZeroDF$REQUESTDATE,1,10), "%Y-%m-%d")
+VisZeroDF$HOUR <- as.numeric(substr(VisZeroDF$REQUESTDATE,12,13))
+VisZeroDF<-subset(VisZeroDF, substr(VisZeroDF$DATE,1,4)=='2015')
